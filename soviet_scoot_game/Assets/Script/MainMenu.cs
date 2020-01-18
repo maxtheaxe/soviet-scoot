d856fd9761696bd8f8c5a1a72efd4919dde7fffe
Seungjae Lee <SJLEE@Seungjae-Macbook-Pro.local>
2020-01-18 12:13:46 -0500

    Added the main menu and end menu. Graphics/background update still needed. Main menu and main game play also connected, but not main game play and endscreen

---
+++ soviet_scoot_game/Assets/Script/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   public void Playgame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ShowRule()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ soviet_scoot_game/Assets/Script/endmenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class endmenu : MonoBehaviour
+{
+    public void Continue()
+    {
+        ;
+        SceneManager.LoadScene(0);
+        // set the right index
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
