c8547d4dcb3d0814ada71cdfc211c083c8af17b4
Seungjae Lee <SJLEE@Seungjae-Macbook-Pro.local>
2020-01-24 13:29:55 -0500

    New updates on caught screen

---
+++ soviet_scoot_game/Assets/CheckCoins.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CheckCoins : MonoBehaviour
+{
+    private GameManager gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameManager.Instance;
+        if (gm.GetCoins() < gm.minCoinCount)
+        {
+            GetComponent<Button>().interactable = false;
+            Debug.Log("Inside the if loop");
+        }
+    }
+}
--- soviet_scoot_game/Assets/Scripts/EndMenu.cs
+++ soviet_scoot_game/Assets/Scripts/EndMenu.cs
@@ -5,8 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class EndMenu : MonoBehaviour
 {
+    private GameManager gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameManager.Instance;
+    }
     public void Continue()
     {
+        gm.SetCoins(gm.GetCoins() - gm.minCoinCount);
         SceneManager.LoadScene("OwenTestScene");
         // set the right name
     }
