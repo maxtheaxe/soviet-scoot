af695348d03ad0824c8b8710e36806179e35a57c
Seungjae Lee <SJLEE@Seungjae-Macbook-Pro.local>
2020-01-28 16:34:53 -0500

    Made storyline, added scene to the main menu

---
+++ soviet_scoot_game/Assets/Scripts/StoryManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class StoryManager : MonoBehaviour
+{
+    private Queue<string> Stories;
+    public Text storytext;
+    public Button SkipButton;
+      
+
+    void Start()
+    {
+        Stories = new Queue<string>();
+    }
+
+    public void StartStory(Story story)
+    {
+        //Debug.Log("start");
+        Stories.Clear();
+        foreach (string sentence in story.stories)
+        {
+            Stories.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (Stories.Count == 1)
+        {
+            EndDialog();
+        }
+        else if (Stories.Count == 0)
+        {
+            Skip();
+            return;
+        }
+        string sentence = Stories.Dequeue();
+        storytext.text = sentence;
+    }
+
+    public void EndDialog()
+    {
+        SkipButton.gameObject.SetActive(false);
+    }
+
+    public void Skip()
+    {
+        //move to the right scene
+        SceneManager.LoadScene("tutorial");
+    }
+}
---
+++ soviet_scoot_game/Assets/Scripts/StoryTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryTrigger : MonoBehaviour
+{
+    public Story story;
+    private bool First = false;
+
+    public void TriggerStory()
+    {
+        if (First == false)
+        {
+            FindObjectOfType<StoryManager>().StartStory(story);
+            First = true;
+        }
+        else
+        {
+            FindObjectOfType<StoryManager>().DisplayNextSentence();
+        }
+    }
+
+}
