082782931d9dfe512e32fbf0ead2239d011ab1c2
Izge Bayyurt <griezwahlm@gmail.com>
2020-01-28 09:26:11 -0500

    some changes to my scene

---
+++ soviet_scoot_game/Assets/Scripts/BuildingRespawn.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingRespawn : MonoBehaviour
+{
+
+    private bool seen;
+    private SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        seen = false;
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sr.isVisible)
+        {
+            seen = true;
+            Debug.Log("seen");
+        }
+
+        if(seen)
+        {
+            Debug.Log("seen2");
+            if (!sr.isVisible)
+            {
+                Debug.Log("unseen");
+                seen = false;
+                transform.position += Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)) + new Vector3(sr.sprite.rect.width,0);
+
+                float randomOffset = Random.value * Screen.width;
+                transform.position += new Vector3(randomOffset, 0);
+
+            }
+        }
+
+
+
+    }
+}
--- soviet_scoot_game/Assets/Scripts/DestroyArrow.cs
+++ soviet_scoot_game/Assets/Scripts/DestroyArrow.cs
@@ -4,17 +4,6 @@ using UnityEngine;
 
 public class DestroyArrow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     void OnTriggerEnter2D(Collider2D col)
     {
